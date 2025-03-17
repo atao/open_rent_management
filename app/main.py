b9a744b545d240b12ee55def6156423f93645a2c
Henry de Labareyre <henry.delabareyre@octave.biz>
2025-03-17 17:24:07 +0100

    - Add use of poetry for project

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from app.api.v1.endpoints import address_controller, property_controller, proper
 from app.models.base import Base
 from fastapi import FastAPI
 from app.core.database import engine
+import uvicorn
 
 app = FastAPI()
 
@@ -17,3 +18,6 @@ app.include_router(user_controller.router, prefix="/api/v1")
 @app.get('/api/healthchecker')
 def read_root():
     return {'message': 'Hello World'}
+
+def start():
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
