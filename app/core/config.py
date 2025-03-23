18f2e0acd673ac6be7a82453aee71f2a1dc14a84
Henry de Labareyre <henry.delabareyre@octave.biz>
2025-03-23 15:36:15 +0100

    - Add some default values in settings

--- app/core/config.py
+++ app/core/config.py
@@ -8,12 +8,12 @@ load_dotenv()
 class Settings:
     PROJECT_NAME: str = "Property Manager"
     API_V1_STR: str = "/api/v1"
-    pg_host = os.getenv("PG_HOST")
-    pg_password = os.getenv("PG_PASSWORD")
-    pg_user = os.getenv("PG_USER")
-    pg_db = os.getenv("PG_DB")
-    pg_port = os.getenv("PG_PORT")
-    secret_key = os.getenv("SECRET_KEY")
+    pg_host = os.getenv("PG_HOST", "localhost")
+    pg_password = os.getenv("PG_PASSWORD", "password")
+    pg_user = os.getenv("PG_USER", "user")
+    pg_db = os.getenv("PG_DB", "database")
+    pg_port = os.getenv("PG_PORT", "5432")
+    secret_key = os.getenv("SECRET_KEY", "your-secret-key")
     algorithm = os.getenv("ALGORITHM", "HS256")
     ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
     REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS", 7))
