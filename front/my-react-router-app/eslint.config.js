c40ef2c65e4705640af8134bc8899608d459bfb1
Henry de Labareyre <henry.delabareyre@octave.biz>
2025-04-25 17:28:00 +0200

    - Update eslinter

--- front/my-react-router-app/eslint.config.js
+++ front/my-react-router-app/eslint.config.js
@@ -4,11 +4,27 @@ import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    plugins: {
+      js,
+    },
+    extends: [
+      "js/recommended",
+      tseslint.configs.recommended,
+      pluginReact.configs.flat.recommended,
+    ],
+    settings: {
+      react: {
+        version: "detect", // Automatically detect the React version
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off", // Disable the rule for React 17+ JSX Transform
+    },
+  },
 ]);
